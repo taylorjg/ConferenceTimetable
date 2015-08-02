25f6b6a0a9d0145c54c036091c5d6cc57973cc80
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-08-02 19:39:35 +0100

    Got a basic version of the program working

--- Timetable1/Program.cs
+++ Timetable1/Program.cs
@@ -13,6 +13,39 @@ namespace Timetable1
     {
         private static void Main(/* string[] args */)
         {
+            var timetables = Test();
+            Console.WriteLine("Number of timetables found: {0}", timetables.Count);
+        }
+
+        // bench :: Int -> Int -> Int -> Int -> Int -> StdGen -> ([Person],[Talk],[TimeTable])
+        // 
+        // bench nslots ntracks ntalks npersons c_per_s gen =
+        //   (persons, talks, timetable persons talks ntracks nslots)
+        //  where
+        //   total_talks = nslots * ntracks
+        // 
+        //   talks = map Talk [1..total_talks]
+        //   persons = mkpersons npersons gen
+        // 
+        //   mkpersons :: Int -> StdGen -> [Person]
+        //   mkpersons 0 g = []
+        //   mkpersons n g = Person ('P':show n) (take c_per_s cs) : rest
+        //         where
+        //           (g1,g2) = split g
+        //           rest = mkpersons (n-1) g2
+        //           cs = nub [ talks !! n | n <- randomRs (0,ntalks-1) g ]
+
+        private static List<Timetable> Test()
+        {
+            var cs = new Talks {1, 2, 3, 4};
+            var testPersons = new[]
+            {
+                new Person("P", new Talks{1,2}),
+                new Person("Q", new Talks{2,3}),
+                new Person("R", new Talks{3,4}),
+                new Person("S", new Talks{1,4})
+            };
+            return Timetable(testPersons, cs, 2, 2);
         }
 
         private static List<Timetable> Timetable(IEnumerable<Person> people, Talks allTalks, int maxTrack, int maxSlot)
